7b735e0a3e1f618bc1c7a597d6bbfabdf3f60a3d
Packetoon <112143000+Packetoon@users.noreply.github.com>
2022-08-28 15:27:03 -0600

    Update Autism.ps1

--- Autism.ps1
+++ Autism.ps1
@@ -1,4 +1,5 @@
-<#File Destination %appdata%\Microsoft\Windows\Start Menu\Programs\Startup #>
+<#File Destination C:\Users\Public\Documents #>
+<#This script will constantly display a message box that wont go away until the person accepts the fact that they have autism. #>
 Add-Type -AssemblyName PresentationFramework
 $result = [System.Windows.MessageBox]::Show('You have Autism', 'Daily Reminder', 'YesNo', 'Information')
 
@@ -8,7 +9,7 @@ switch ($result){
     }
     'No'{
         do{
-            $Result = [System.Windows.MessageBox]::Show('Admit it, you have autism!!', 'Daily Reminder','YesNo','Information')
+            $Result = [System.Windows.MessageBox]::Show('Stop lying to yourself', 'Daily Reminder','YesNo','Information')
             if ($Result -eq 'Yes'){
                 [System.Windows.MessageBox]::Show('Thank you for your submission, sending results to Hot Girls on campus.', 'Thank You!!','OK','Information')
                 break
