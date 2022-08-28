a94c62a45d1ba56b2041fe4399bfb2eddbccc9c6
Packetoon <112143000+Packetoon@users.noreply.github.com>
2022-08-28 15:40:29 -0600

    Delete Start.cmd

--- Start.cmd
+++
@@ -1,2 +0,0 @@
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-powershell -windowstyle hidden C:\Users\Public\Documents\Autism.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
